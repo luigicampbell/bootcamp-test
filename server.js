95d90d156f624bba6ec958e2d1a023d2a02b766c
luca_dgaf <luigi@github.com>
2024-09-16 20:19:41 -0700

    added catch all route for unsupported paths

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+app.get('*', (req, res) => {
+  res.status(404).send('Uh Oh, you are lost!')
+})
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
